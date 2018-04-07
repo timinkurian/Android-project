2eb16844d2d846f6cc4735d0de45b7f19609ddec
timinkurian <33921489+timinkurian@users.noreply.github.com>
2018-04-07 14:50:27 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,56 +1,35 @@
-package com.example.timin.pdfadmin;
+package com.example.timin.mcanotes;
 
-import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import net.gotev.uploadservice.MultipartUploadRequest;
-import net.gotev.uploadservice.UploadNotificationConfig;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-    Spinner sp1,sp2;
-    //Declaring views
-    private Button buttonChoose;
-    private Button buttonUpload;
-
-    private EditText editText;
-
-    public static final String UPLOAD_URL = "https://conserving-gravel.000webhostapp.com/misc/timin/uploadPdf.php";
+Spinner sp1,sp2;
+    String courseval,semval;
     public static final String PDF_FETCH_URL = "https://conserving-gravel.000webhostapp.com/misc/timin/viewAll.php";
-
-    ImageView imageView;
-
-    //Image request code
-    private int PICK_PDF_REQUEST = 1;
-
-    //storage permission code
-    private static final int STORAGE_PERMISSION_CODE = 123;
-
-    //Bitmap to get image from gallery
-    private Bitmap bitmap;
-
-    //Uri to store the image uri
-    private Uri filePath;
-
+    PdfAdapter pdfAdapter;
     //ListView to show the fetched Pdfs from the server
     ListView listView;
 
@@ -60,20 +39,18 @@ public class MainActivity extends AppCompatActivity {
     //Progress bar to check the progress of obtaining pdfs
     ProgressDialog progressDialog;
 
-    //pdf adapter
-    String semval,courseval;
+    //an array to hold the different pdf objects
+    ArrayList<Pdf> pdfList = new ArrayList<Pdf>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         sp2= (Spinner) findViewById(R.id.sp2);
         sp1= (Spinner) findViewById(R.id.sp1);
-        //Requesting storage permission
-        requestStoragePermission();
+        buttonFetch=(Button) findViewById(R.id.buttonFetchPdf) ;
+        listView=(ListView)findViewById(R.id.listView);
+        progressDialog=new ProgressDialog(MainActivity.this);
 
-        //Initializing views
-        buttonChoose = (Button) findViewById(R.id.buttonChoose);
-        buttonUpload = (Button) findViewById(R.id.buttonUpload);
         String[] arraySpinner = new String[] {
                 "S1", "S2", "S3", "S4", "S5"
         };
@@ -142,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
         sp2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -153,94 +131,96 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-        buttonChoose.setOnClickListener(new View.OnClickListener() {
+        buttonFetch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showFileChooser();
+                getPdfs();
             }
         });
-
-        buttonUpload.setOnClickListener(new View.OnClickListener() {
+        //setting listView on item click listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                uploadMultipart();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+                Pdf pdf = (Pdf) parent.getItemAtPosition(position);
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(pdf.getUrl()));
+                startActivity(intent);
+
             }
         });
+    }
+    private void getPdfs() {
 
+        progressDialog.setMessage("Fetching Pdfs... Please Wait");
+        progressDialog.show();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, PDF_FETCH_URL,
+
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        progressDialog.dismiss();
+                        pdfList.clear();
+                        try {
+                            JSONArray obj = new JSONArray(response);
+                            /*Toast.makeText(MainActivity.this, obj.getString("message"), Toast.LENGTH_SHORT).show();
+
+                            JSONArray jsonArray = obj.getJSONArray("pdfs");*/
+
+                            for (int i = 0; i < obj.length(); i++) {
+
+                                //Declaring a json object corresponding to every pdf object in our json Array
+                                JSONObject jsonObject = obj.getJSONObject(i);
+                                //Declaring a Pdf object to add it to the ArrayList  pdfList
+                                Pdf pdf = new Pdf();
+                                String pdfCategory = jsonObject.getString("category");
+                                String pdfName = jsonObject.getString("name");
+                                String pdfUrl = "https://conserving-gravel.000webhostapp.com/misc/timin/"+jsonObject.getString("path");
+
+                                if (pdfName.equalsIgnoreCase(semval)){
+                                    if (pdfCategory.equalsIgnoreCase(courseval)){
+                                        pdf.setName(pdfName);
+                                        pdf.setUrl(pdfUrl);
+                                        pdf.setCategory(pdfCategory);
+                                        pdfList.add(pdf);
+
+                                    }
+                                }
+
+                            }
+
+                            pdfAdapter = new PdfAdapter(MainActivity.this, R.layout.list_layout, pdfList);
+
+                            listView.setAdapter(pdfAdapter);
+
+                            pdfAdapter.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+
+
+        );
+
+        RequestQueue request = Volley.newRequestQueue(this);
+        request.add(stringRequest);
 
     }
 
-
-
-   /*
-    * This is the method responsible for image upload
-    * We need the full image path and the name for the image in this method
-    * */
-
-
-    public void uploadMultipart() {
-        //getting name for the pdf
-        //String name = editText.getText().toString().trim();
-
-        //getting the actual path of the pdf
-        String path = FilePath.getPath(this, filePath);
-
-        if (path == null) {
-
-            Toast.makeText(this, "Please move your .pdf file to internal storage and retry", Toast.LENGTH_LONG).show();
-        } else {
-            //Uploading code
-            try {
-                String uploadId = UUID.randomUUID().toString();
-
-                //Creating a multi part request
-                new MultipartUploadRequest(this, uploadId, UPLOAD_URL)
-                        .addFileToUpload(path, "file") //Adding file
-                        .addParameter("name", semval) //Adding text parameter to the request
-                        .addParameter("category",courseval)
-                        .setNotificationConfig(new UploadNotificationConfig())
-                        .setMaxRetries(2)
-                        .startUpload(); //Starting the upload
-
-            } catch (Exception exc) {
-                Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-
-    //method to show file chooser
-    private void showFileChooser() {
-        Intent intent = new Intent();
-        intent.setType("application/pdf");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(Intent.createChooser(intent, "Select Pdf"), PICK_PDF_REQUEST);
-    }
-
-    //handling the ima chooser activity result
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == PICK_PDF_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            filePath = data.getData();
-
-        }
-    }
-
-    //Requesting permission
-    private void requestStoragePermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-            return;
-
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            //If the user has denied the permission previously your code will come to this block
-            //Here you can explain why you need this permission
-            //Explain here why you need this permission
-        }
-        //And finally ask for the permission
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
-    }
 }
+
---
+++ Pdf.java
@@ -0,0 +1,34 @@
+package com.example.timin.mcanotes;
+
+
+
+public class Pdf {
+
+    private String url;
+    private String name;
+    private String category;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
